0bd91c8c971723a978fbb4b63fe5fe492c3bb97c
Koby Fink <finkkoby@icloud.com>
2024-02-12 12:31:38 -0500

    complete lab

--- javascript/find_shortest_string.js
+++ javascript/find_shortest_string.js
@@ -1,5 +1,11 @@
 function findShortestString(arr) {
-  // type your code here
+  let shortestString = arr[0];
+  for (let string of arr) {
+    if (shortestString.length > string.length) {
+      shortestString = string;
+    }
+  }
+  return shortestString;
 }
 
 if (require.main === module) {
@@ -24,3 +30,7 @@ module.exports = findShortestString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+// declare variable to hold shortest string and assign it arr[0]
+// iterate through each array and compare the length to the existing length of our answer object
+// if it is shorter than the string assigned to shortest string, update the answer object
+// return shortest string variable
